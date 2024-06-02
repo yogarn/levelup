352704e53d7e892376337bd5741b418f9125ef56
Yoga Raditya <radityanalaa@gmail.com>
2024-06-02 19:16:26 +0700

    chore: Add redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,21 @@ import { PrismaModule } from './database/prisma/prisma.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { DiariesModule } from './models/diaries/diaries.module';
 import { RolesModule } from './models/roles/roles.module';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './models/roles/roles.guard';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
+import { RedisOptions } from './configs/redis-options.constants';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthenticationModule, DiariesModule, RolesModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    AuthenticationModule,
+    DiariesModule,
+    RolesModule,
+    CacheModule.registerAsync(RedisOptions),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
   getHello(): string {
     return 'Hello World!';
   }
